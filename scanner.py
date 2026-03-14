a970f5eea491821de1810978a2752fbde3656631
m1rR <m1rrozz@icloud.com>
2026-03-14 14:49:14 +0100

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,22 +1,46 @@
 import socket
+import threading
+from queue import Queue
 
-def scan_port(ip, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.5)
+print('-' * 30)
+target = input(str('Enter target IP: '))
+queue = Queue()
+open_ports = []
 
+def scan_port(port):
     try:
-        result = s.connect_ex((ip, port))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.5)
+        result = s.connect_ex((target, port))
+
         if result == 0:
-            print(f'Port {port} is OPEN')
+            open_ports.append(port)
+            try:
+                banner = s.recv(1024).decode().strip()
+                if banner:
+                    print(f'[+] Port {port} is OPEN | Service: {banner}')
+            except:
+                print(f'[+] Port {port} is OPEN')
         s.close()
     except:
         pass
 
 
-target = 'google.com'
-print(f'Scanning target: {target}')
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        scan_port(port)
+        queue.task_done()
 
 for port in range(1, 1025):
-    scan_port(target, port)
+    queue.put(port)
 
+for _ in range(100):
+    t = threading.Thread(target=worker)
+    t.start()
+
+
+queue.join()
+
+print('-' * 30)
 print('Scan finished.')
