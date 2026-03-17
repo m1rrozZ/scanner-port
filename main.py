340964252b6d6af4dd1a38b2ca362aa3521d62bb
m1rR <m1rrozz@icloud.com>
2026-03-17 14:48:35 +0100

    change main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import socket
 import sys
 import time
@@ -23,22 +24,58 @@ def print_logo():
 
 
 def scan_port(host, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.settimeout(0.5)
-        result = sock.connect_ex((host, port))
-        if result == 0:
-            return port
-    return None
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(1)
+            if sock.connect_ex((host, port)) == 0:
+                try:
+                    service = socket.getservbyport(port, "tcp")
+                except:
+                    service = "uknown"
+                return port, service
+    except:
+        pass
+    return None, None
 
 
-print_logo()
-target = input("\n[TARGET] Enter target host: ")
-port_to_scan = range(1, 1025)
-print(f"[INFO] Scanning {target}...")
+def get_args():
+    parser = argparse.ArgumentParser(description="Port scanner")
+    parser.add_argument("target", help="Target host")
+    parser.add_argument(
+        "-p", "--ports", default="1-1024", help="Port range (ex: 1-1000)"
+    )
+    parser.add_argument(
+        "-t", "--threads", default=100, type=int, help="Number of threads to use"
+    )
+    return parser.parse_args()
 
-with ThreadPoolExecutor(max_workers=100) as executor:
-    result = executor.map(lambda p: scan_port(target, p), port_to_scan)
 
-for port in result:
-    if port:
-        print(f"[INFO] Port {port} is open")
+if __name__ == "__main__":
+    args = get_args()
+    print_logo()
+
+    try:
+        start_port, end_port = map(int, args.ports.split("-"))
+        port_to_scan = range(start_port, end_port + 1)
+    except ValueError:
+        print("[ERROR] Invalid port range")
+        sys.exit(1)
+
+    print(f"\n[INFO] Scanning ports {start_port}-{end_port} on {args.target}")
+    print(f"[INFO] Using {len(port_to_scan)} threads\n")
+    print("-" * 30)
+
+    found_any = False
+
+    with ThreadPoolExecutor(max_workers=args.threads) as executor:
+        # result
+        results = executor.map(lambda p: scan_port(args.target, p), port_to_scan)
+
+        for port, service in results:
+            if port:
+                print(f"[INFO] Port: {port} | Service: ({service})")
+                found_any = True
+    if not found_any:
+        print("\n[INFO] No open ports found")
+
+    print("\n[INFO] Scan complete")
